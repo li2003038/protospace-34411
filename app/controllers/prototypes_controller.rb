b04d35d14c9e21654cd3ed4d2eae5ad7fa2fb3c7
li2003038 <lio_817@yahoo.co.jp>
2021-02-12 17:45:43 +0900

    Revert "before_actionの修正"
    This reverts commit e00778ad04b5ed6635499acc07332f73aa30ea6e.

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :edit, :destroy]
+  before_action :authenticate_user!, except: [:create, :edit, :destroy]
   before_action :move_to_index, only: [:edit]
 
   def index
